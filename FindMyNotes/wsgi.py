5466e7295d8e21b670cdd0194b4bd789523f1e01
Rishabh Nahar <rishabhnahar1202@gmail.com>
2022-09-16 23:47:08 +0530

    Heroku set up

--- FindMyNotes/wsgi.py
+++ FindMyNotes/wsgi.py
@@ -9,7 +9,8 @@
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import DjangoWhiteNoise
+# from whitenoise import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FindMyNotes.settings")
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
+# application = DjangoWhiteNoise(get_wsgi_application())
