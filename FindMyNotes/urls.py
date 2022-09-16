9cfdce7b61c901436830d5a8d518e4333b46af81
Rishabh Nahar <rishabhnahar1202@gmail.com>
2022-09-17 00:46:52 +0530

    Heroku set up

--- FindMyNotes/urls.py
+++ FindMyNotes/urls.py
@@ -14,17 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import include, url
 
 from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('backend.app_url')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
