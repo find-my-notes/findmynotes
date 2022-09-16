4aba7222c80375a629677ac6f801fbe09acd2dac
Rishabh Nahar <rishabhnahar1202@gmail.com>
2022-09-17 00:26:04 +0530

    Heroku set up

--- FindMyNotes/urls.py
+++ FindMyNotes/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
+# from django.views.static import serve
+# from django.conf.urls import url
 
 from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('backend.app_url')),
+    # path('',include('backend.app_url')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
