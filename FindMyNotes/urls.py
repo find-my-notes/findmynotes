09bbe6e8f8d9c772bf50b0f2184b97a5bbb9c0ed
Rishabh Nahar <rishabhnahar1202@gmail.com>
2022-09-17 00:29:03 +0530

    Heroku set up

--- FindMyNotes/urls.py
+++ FindMyNotes/urls.py
@@ -23,7 +23,7 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include('backend.app_url')),
+    path('',include('backend.app_url')),
 
     # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
