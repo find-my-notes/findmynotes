359a649458f2c3b41950c5dad7f0e325eda6269c
Rishabh Nahar <rishabhnahar1202@gmail.com>
2022-09-17 18:43:02 +0530

    Heroku set up - admin site

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.models import bookmarked_files, file_likes, file_upload, reported_file, searched_file, user_details
+
+admin.site.register(user_details)
+admin.site.register(bookmarked_files)
+admin.site.register(file_likes)
+admin.site.register(file_upload)
+admin.site.register(reported_file)
+admin.site.register(searched_file)
