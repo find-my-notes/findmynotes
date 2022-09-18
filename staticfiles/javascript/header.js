aa1c98634de94e0c5a8899a44e2a4ca358498d06
Rishabh Nahar <rishabhnahar1202@gmail.com>
2022-09-18 13:37:19 +0530

    Navbar logo and favicon

--- staticfiles/javascript/header.js
+++
@@ -1,6 +0,0 @@
-function toggleDropdownLinks(){
-    var Dropdown_links = document.getElementById("dropdown_links")
-    var angle_down_arrow = document.getElementById("Dropdown_angle_down");
-    angle_down_arrow.classList.toggle('Dropdown_angle_rotate')
-    Dropdown_links.classList.toggle('height-0')
-}
